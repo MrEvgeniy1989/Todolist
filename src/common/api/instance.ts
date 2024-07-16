47cc6af2b84b9391890b8805e3e912b2d6db2e8a
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-07-16 15:24:29 +0300

    fix: Added API-KEY.

--- src/common/api/instance.ts
+++ src/common/api/instance.ts
@@ -3,4 +3,7 @@ import axios from "axios"
 export const instance = axios.create({
   withCredentials: true,
   baseURL: "https://social-network.samuraijs.com/api/1.1",
+  headers: {
+    "API-KEY": "af0e3219-367b-49e0-a604-f544cf649725"
+  }
 })
